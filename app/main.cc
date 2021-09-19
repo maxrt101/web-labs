90af7499d2ac8cb80b03f0851415dd65fd62c8ad
Maxym Tkachuk <s2m4f6@gmail.com>
2021-09-19 22:06:06 +0300

    Implemented DELETE and GET by id

--- app/main.cc
+++ app/main.cc
@@ -6,15 +6,16 @@
  * @author maxrt101
  */
 
-#include "mrt/args/args.h"
-#include "mrt/server/server.h"
-#include "mrt/server/debug/log.h"
-#include "nlohmann/json.hpp"
-
+#include <stdexcept>
 #include <iostream>
 #include <string>
 #include <atomic>
 #include <map>
+
+#include "mrt/args/args.h"
+#include "mrt/server/server.h"
+#include "mrt/server/debug/log.h"
+#include "nlohmann/json.hpp"
 
 #include "car.h"
 
@@ -35,21 +36,44 @@ int main(int argc, const char ** argv) {
     http::ResponseCode code = http::OK;
     nlohmann::json response = nlohmann::json::array();
 
-    for (auto& car : g_cars) {
+    int id = -1;
+
+    try {
+      id = std::stoi(request.header.params.at("id"));
+    } catch (std::exception& e) {
+      for (auto& car : g_cars) {
+        try {
+          response.push_back(car.second.to_json());
+          response[response.size()-1]["id"] = car.first;
+        } catch (std::exception& e) {
+          log::error("Json Parse Error: %s", e.what());
+          code = http::INTERNAL_SERVER_ERROR;
+          response = nlohmann::json();
+          response["error"] = "Json Parse Error";
+          break;
+        }
+      }
+    }
+
+    if (id != -1) {
       try {
-        response.push_back(car.second.to_json());
+        Car car = g_cars.at(id);
+        response = car.to_json();
+        response["id"] = id;
+      } catch (std::out_of_range& e) {
+        log::error("Invalid ID: %s", e.what());
+        code = http::BAD_REQUEST;
+        response["error"] = "Invalid ID";
       } catch (std::exception& e) {
         log::error("Json Parse Error: %s", e.what());
         code = http::INTERNAL_SERVER_ERROR;
-        response = nlohmann::json();
         response["error"] = "Json Parse Error";
-        break;
       }
     }
 
     return http::Response(code).setContent("text/json", response.dump());
   }});
-
+  
   server.addEndpoint({"/api/car", http::Method::POST, [](auto request) {
     http::ResponseCode code = http::OK;
     nlohmann::json response;
@@ -63,6 +87,36 @@ int main(int argc, const char ** argv) {
       code = http::INTERNAL_SERVER_ERROR;
       response = nlohmann::json();
       response["error"] = "Json Parse Error";
+    }
+
+    return http::Response(code).setContent("text/json", response.dump());
+  }});
+
+  server.addEndpoint({"/api/car", http::Method::DELETE, [](auto request) {
+    http::ResponseCode code = http::OK;
+    nlohmann::json response;
+
+    int id = -1;
+    Car car;
+
+    try {
+      id = std::stoi(request.header.params.at("id"));
+      car = g_cars.at(id);
+      g_cars.erase(id);
+    } catch (std::exception& e) {
+      log::error("Invalid ID: %s", e.what());
+      code = http::BAD_REQUEST;
+      response["error"] = "Invalid ID";
+    }
+
+    if (id != -1) {
+      try {
+        response = car.to_json();
+      } catch (std::exception& e) {
+        log::error("Invalid Object: %s", e.what());
+        code = http::INTERNAL_SERVER_ERROR;
+        response["error"] = "Invalid Object";
+      }
     }
 
     return http::Response(code).setContent("text/json", response.dump());
